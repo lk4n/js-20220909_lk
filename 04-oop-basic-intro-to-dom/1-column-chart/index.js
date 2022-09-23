771c1e41d6c29d3867be318e1a3a22fba2964f11
Lex K <lex.kachan@gmail.com>
2022-09-23 20:53:59 +0300

    Update remove, destroy

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -30,11 +30,14 @@ export default class ColumnChart {
   }
 
   remove() {
-    this.element = null;
+    if (this.element) {
+      this.element.remove();
+    }
   }
 
   destroy() {
     this.remove();
+    this.element = null;
   }
 
   getTemplate() {
@@ -51,25 +54,31 @@ export default class ColumnChart {
             ${this.value}
           </div>
           <div data-element="body" class="column-chart__chart">
-            <div style="--value: 12" data-tooltip="25%"></div>
-            <div style="--value: 16" data-tooltip="33%"></div>
-            <div style="--value: 8" data-tooltip="17%"></div>
-            <div style="--value: 33" data-tooltip="67%"></div>
-            <div style="--value: 14" data-tooltip="29%"></div>
-            <div style="--value: 6" data-tooltip="13%"></div>
-            <div style="--value: 25" data-tooltip="50%"></div>
-            <div style="--value: 8" data-tooltip="17%"></div>
-            <div style="--value: 6" data-tooltip="13%"></div>
-            <div style="--value: 18" data-tooltip="38%"></div>
-            <div style="--value: 50" data-tooltip="100%"></div>
-            <div style="--value: 41" data-tooltip="83%"></div>
-            <div style="--value: 35" data-tooltip="71%"></div>
-            <div style="--value: 41" data-tooltip="83%"></div>
-            <div style="--value: 4" data-tooltip="8%"></div>
-            <div style="--value: 2" data-tooltip="4%"></div>
+            ${this.getCharts}
           </div>
         </div>
       </div>
+    `;
+  }
+
+  getCharts() {
+    return `
+      <div style="--value: 12" data-tooltip="25%"></div>
+      <div style="--value: 16" data-tooltip="33%"></div>
+      <div style="--value: 8" data-tooltip="17%"></div>
+      <div style="--value: 33" data-tooltip="67%"></div>
+      <div style="--value: 14" data-tooltip="29%"></div>
+      <div style="--value: 6" data-tooltip="13%"></div>
+      <div style="--value: 25" data-tooltip="50%"></div>
+      <div style="--value: 8" data-tooltip="17%"></div>
+      <div style="--value: 6" data-tooltip="13%"></div>
+      <div style="--value: 18" data-tooltip="38%"></div>
+      <div style="--value: 50" data-tooltip="100%"></div>
+      <div style="--value: 41" data-tooltip="83%"></div>
+      <div style="--value: 35" data-tooltip="71%"></div>
+      <div style="--value: 41" data-tooltip="83%"></div>
+      <div style="--value: 4" data-tooltip="8%"></div>
+      <div style="--value: 2" data-tooltip="4%"></div>
     `;
   }
 }
