47b142ef8ebe89cbb450084f811bf3b75b0d86b6
Lex K <lex.kachan@gmail.com>
2022-09-24 00:24:33 +0300

    Add charts getter

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -54,31 +54,23 @@ export default class ColumnChart {
             ${this.value}
           </div>
           <div data-element="body" class="column-chart__chart">
-            ${this.getCharts}
+            ${this.charts}
           </div>
         </div>
       </div>
     `;
   }
 
-  getCharts() {
-    return `
-      <div style="--value: 12" data-tooltip="25%"></div>
-      <div style="--value: 16" data-tooltip="33%"></div>
-      <div style="--value: 8" data-tooltip="17%"></div>
-      <div style="--value: 33" data-tooltip="67%"></div>
-      <div style="--value: 14" data-tooltip="29%"></div>
-      <div style="--value: 6" data-tooltip="13%"></div>
-      <div style="--value: 25" data-tooltip="50%"></div>
-      <div style="--value: 8" data-tooltip="17%"></div>
-      <div style="--value: 6" data-tooltip="13%"></div>
-      <div style="--value: 18" data-tooltip="38%"></div>
-      <div style="--value: 50" data-tooltip="100%"></div>
-      <div style="--value: 41" data-tooltip="83%"></div>
-      <div style="--value: 35" data-tooltip="71%"></div>
-      <div style="--value: 41" data-tooltip="83%"></div>
-      <div style="--value: 4" data-tooltip="8%"></div>
-      <div style="--value: 2" data-tooltip="4%"></div>
-    `;
+  get charts() {
+    const maxValue = Math.max(...this.data);
+    const scale = this.chartHeight / maxValue;
+
+    return this.data
+      .map(function (elem) {
+        return `<div style="--value: ${String(
+          Math.floor(elem * scale)
+        )}" data-tooltip="${((elem / maxValue) * 100).toFixed(0)}%"></div>`;
+      })
+      .join("");
   }
 }
