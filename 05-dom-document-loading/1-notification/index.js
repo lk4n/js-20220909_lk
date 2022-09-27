a9c5cfb9f7ff979e22c20edb259918b113aa65e8
Lex K <lex.kachan@gmail.com>
2022-09-27 19:26:54 +0300

    Notification init commit

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,48 @@
 export default class NotificationMessage {
+  constructor(message = "", { duration = 2000, type = "success" } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
 
+    this.render();
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" 
+        style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">success</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+
+    wrapper.innerHTML = this.template;
+    this.element = wrapper.firstElementChild;
+  }
+
+  show() {
+    const parent = document.body;
+
+    parent.append(this.element);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
