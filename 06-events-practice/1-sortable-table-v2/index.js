61a9c97ef85685b82b71cfb8894b6a36142895fe
Lex K <lex.kachan@gmail.com>
2022-10-02 15:49:18 +0300

    Init sortableTable v2

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,166 @@
 export default class SortableTable {
-  constructor(headersConfig, {
-    data = [],
-    sorted = {}
-  } = {}) {
+  element;
+  subElements = {};
 
+  constructor(
+    headerConfig = [],
+    {
+      data = [],
+      sorted = {
+        id: headerConfig.find(function (item) {
+          return item.sortable;
+        }).id,
+        order: "asc",
+      },
+    } = {}
+  ) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+    this.sorted = sorted;
+
+    this.render();
+  }
+
+  get template() {
+    return `
+      <div class="sortable-table">
+        ${this.templateTableHeader}
+        ${this.templateTableBody}
+      </div>
+    `;
+  }
+
+  get templateTableHeader() {
+    return `
+      <div data-element="header" class="sortable-table__header sortable-table__row">
+        ${this.headerConfig
+          .map(function (configItem) {
+            const { id, title, sortable } = configItem;
+
+            return `
+              <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}">
+                <span>${title}</span>
+                <span data-element="arrow" class="sortable-table__sort-arrow">
+                <span class="sort-arrow"></span>
+                </span>
+              </div>
+            `;
+          })
+          .join("")}
+      </div>
+    `;
+  }
+
+  get templateTableBody() {
+    return `
+      <div data-element="body" class="sortable-table__body">
+        ${this.getTemplateTableRows(this.data)}
+      </div>
+    `;
+  }
+
+  getTemplateTableRows(data = []) {
+    return data
+      .map(function (dataItem) {
+        return `
+          <a href="/products/${dataItem.id}" class="sortable-table__row">
+            ${this.getTemplateTableRow(dataItem)}
+          </a>
+       `;
+      }, this)
+      .join("");
+  }
+
+  getTemplateTableRow(dataItem) {
+    const cells = this.headerConfig.map(function ({ id, template }) {
+      return { id, template };
+    });
+
+    return cells
+      .map(function ({ id, template }) {
+        return template
+          ? template(dataItem[id])
+          : `<div class="sortable-table__cell">${dataItem[id]}</div>`;
+      })
+      .join("");
+  }
+
+  sort(field, order) {
+    const sortedData = this.sortData(field, order);
+    const allColumns = this.element.querySelectorAll(
+      ".sortable-table__cell[data-id]"
+    );
+    const currentColumn = this.element.querySelector(
+      `.sortable-table__cell[data-id="${field}"]`
+    );
+
+    //Markup: clear previous arrows and set for current column only
+    allColumns.forEach(function (column) {
+      column.dataset.order = "";
+    });
+    currentColumn.dataset.order = order;
+
+    this.subElements.body.innerHTML = this.getTemplateTableRows(sortedData);
+  }
+
+  sortData(field, order) {
+    const dataToSort = Array.from(this.data);
+    const column = this.headerConfig.find(function (item) {
+      return item.id === field;
+    });
+    const { sortType } = column;
+    const direction = {
+      asc: 1,
+      desc: -1,
+    };
+    const directionSign = direction[order];
+
+    return dataToSort.sort(function (a, b) {
+      switch (sortType) {
+        case "number":
+          return directionSign * (a[field] - b[field]);
+        case "string":
+          return directionSign * a[field].localeCompare(b[field], ["ru", "en"]);
+        default:
+          throw new Error("Sort type not defined");
+      }
+    });
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+
+    wrapper.innerHTML = this.template;
+
+    const element = wrapper.firstElementChild;
+
+    this.element = element;
+
+    this.subElements = this.getSubElements(element);
+  }
+
+  getSubElements(element) {
+    const result = {};
+    const elements = element.querySelectorAll("[data-element]");
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+
+      result[name] = subElement;
+    }
+
+    return result;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
   }
 }
