f34f219fe920c39265867253e35908052ba5cf1b
Lex K <lex.kachan@gmail.com>
2022-10-07 19:14:58 +0300

    tooltip init

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,33 @@
 class Tooltip {
-  initialize () {
+  element;
+  static refer;
 
+  constructor() {
+    if (Tooltip.refer) {
+      Tooltip.refer.remove();
+    }
+    Tooltip.refer = this;
+  }
+
+  onPointerOver() {}
+
+  onPointerOut() {}
+
+  initEventListeners() {}
+
+  initialize() {}
+
+  render() {}
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  detroy() {
+    this.remove();
+    this.element = null;
   }
 }
 
